76a692fc401e3873845dcc397ee5bd133ab12e3d
Sayak-tech2 <sayak.chakraborty001@gmail.com>
2025-06-23 12:44:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import CryptoTable from "./CryptoTable";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-6">
+      <h1 className="text-3xl font-bold text-center mb-8">CryptoGenius</h1>
+      <CryptoTable />
+    </div>
+  );
+}
+
+export default App;
